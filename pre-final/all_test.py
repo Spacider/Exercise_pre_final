7564e0ae0ed29d5f6d8948761d43d1689d701a18
Spacider <gary_stu@163.com>
2020-04-26 01:01:09 +1000

    update

--- pre-final/all_test.py
+++ pre-final/all_test.py
@@ -2,36 +2,14 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'Gary'
 
-def rectangle(width, height):
-    '''
-    ab
-    dc
-    ef
-    '''
-    result_list = []
-    a_aksii = ord('a') - 1
-    for i in range(height):
-        single_str = ''
-        for j in range(width):
-            a_aksii = a_aksii + 1
-            if a_aksii > ord('z'):
-                a_aksii = ord('a')
-            print(a_aksii)
-            single_str = single_str + chr(a_aksii)
-        result_list.append(single_str)
-    print(result_list)
-    # ['ab', 'cd', 'ef']
-    for index in range(len(result_list)):
-        if index % 2 == 0:
-            print(result_list[index])
-        else:
-            print(result_list[index][::-1])
+def subnumbers_whose_digits_add_up_to(number, sum_of_digits):
 
+    pass
 
 
 
 
 if __name__ == '__main__':
-    print(rectangle(17, 4))
+    subnumbers_whose_digits_add_up_to(1234, 5)
 
 
