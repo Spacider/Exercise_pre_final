c01f080f86893d9713c8bdb5a9cd862132db1052
Spacider <gary_stu@163.com>
2020-04-26 00:56:22 +1000

    update

--- pre-final/exercise6.py
+++ pre-final/exercise6.py
@@ -2,11 +2,26 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'Gary'
 
+# ord(c) returns the encoding of character c. # chr(e) returns the character encoded by e.
 def rectangle(width, height):
     '''
+    Displays a rectangle by outputting lowercase letters, starting with a,
+    in a "snakelike" manner, from left to right, then from right to left,
+    then from left to right, then from right to left, wrapping around when z is reached.
+    >>> rectangle(1, 1)
+    a
+    >>> rectangle(2, 3)
     ab
     dc
     ef
+    >>> rectangle(3, 2)
+    abc
+    fed
+    >>> rectangle(17, 4)
+    abcdefghijklmnopq
+    hgfedcbazyxwvutsr
+    ijklmnopqrstuvwxy
+    ponmlkjihgfedcbaz
     '''
     result_list = []
     a_aksii = ord('a') - 1
@@ -16,22 +31,11 @@ def rectangle(width, height):
             a_aksii = a_aksii + 1
             if a_aksii > ord('z'):
                 a_aksii = ord('a')
-            print(a_aksii)
             single_str = single_str + chr(a_aksii)
         result_list.append(single_str)
-    print(result_list)
     # ['ab', 'cd', 'ef']
     for index in range(len(result_list)):
         if index % 2 == 0:
             print(result_list[index])
         else:
             print(result_list[index][::-1])
-
-
-
-
-
-if __name__ == '__main__':
-    print(rectangle(17, 4))
-
-
